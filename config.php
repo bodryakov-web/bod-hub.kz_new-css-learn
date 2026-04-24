780fb02b96d1a0523c33c30c2ce9adf3d7d98615
bodryakov.web@gmail.com <bodryakov.web@gmail.com>
2026-04-24 14:13:46 +0500

    *

--- config.php
+++ config.php
@@ -39,7 +39,7 @@ if (defined('ENVIRONMENT') && ENVIRONMENT === 'development') {
 } else {
     // Production настройки для хостинга
     error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
-    ini_set('display_errors', 1); // Временно включено для отладки
+    ini_set('display_errors', 0);
     ini_set('log_errors', 1);
     ini_set('error_log', __DIR__ . '/error.log');
 }
